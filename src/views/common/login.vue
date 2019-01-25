2ae85c44b22030038068d600f926db6ffeb36de0
389091912 <389091912@qq.com>
2019-01-26 01:34:08 +0800

    更新样式

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -3,22 +3,22 @@
     <div class="site-content__wrapper">
       <div class="site-content">
         <div class="brand-info">
-          <h2 class="brand-info__text">renren-fast-vue</h2>
-          <p class="brand-info__intro">renren-fast-vue基于vue、element-ui构建开发，实现renren-fast后台管理前端功能，提供一套更优的前端解决方案。</p>
+          <h2 class="brand-info__text">宏鑫玻璃有限公司</h2>
+          <p class="brand-info__intro">宏鑫玻璃有限公司生产管理系统，提供一套更优的生产方案。</p>
         </div>
         <div class="login-main">
           <h3 class="login-title">管理员登录</h3>
           <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()" status-icon>
             <el-form-item prop="userName">
-              <el-input v-model="dataForm.userName" placeholder="帐号"></el-input>
+              <el-input v-model="dataForm.userName" maxlength="18" placeholder="帐号"></el-input>
             </el-form-item>
             <el-form-item prop="password">
-              <el-input v-model="dataForm.password" type="password" placeholder="密码"></el-input>
+              <el-input v-model="dataForm.password" maxlength="18" type="password" placeholder="密码"></el-input>
             </el-form-item>
             <el-form-item prop="captcha">
               <el-row :gutter="20">
                 <el-col :span="14">
-                  <el-input v-model="dataForm.captcha" placeholder="验证码">
+                  <el-input  maxlength="5" v-model="dataForm.captcha" placeholder="验证码">
                   </el-input>
                 </el-col>
                 <el-col :span="10" class="login-captcha">
